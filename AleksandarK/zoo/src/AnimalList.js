f782baf031c43a772def9fcd25303d32e6fb5c99
aleksandarkacar <kacar95@gmail.com>
2023-03-01 21:06:38 +0100

    Peti Zadatak Zavrsen

--- AleksandarK/zoo/src/AnimalList.js
+++ AleksandarK/zoo/src/AnimalList.js
@@ -23,9 +23,58 @@ function AnimalList() {
     setAnimals(newAnimals);
   };
 
+  const addAnimal = (animal) => {
+    setAnimals([...animals, animal]);
+  };
+
+  const [type, setType] = useState("");
+  const [name, setName] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents default behaviour of form submit action
+    // console.log({ type: type, name: type, dateOfBirth: dateOfBirth });
+    addAnimal({ type: type, name: type, dateOfBirth: dateOfBirth }); // Sending data   to the server. (Will be covered sometime later)
+  };
+
   return (
     <div className="App">
       <h1>Animals:</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Type:
+          <input
+            placeholder="Type"
+            type="text"
+            value={type}
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Age:
+          <input
+            placeholder="Age"
+            type="text"
+            value={dateOfBirth}
+            onChange={(e) => {
+              setDateOfBirth(e.target.value);
+            }}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
       <table className="App">
         <thead>
           <tr>
